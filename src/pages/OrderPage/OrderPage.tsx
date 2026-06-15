6c96d4e382fc9ecd1a2a492e90150387f3bdf13e
minus1zub <minus1zub@users.noreply.github.com>
2026-06-15 22:56:16 +0900

    Rename order next step button

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -201,7 +201,7 @@ export const OrderPage = () => {
           Очистить
         </AnimatedButton>
         <AnimatedButton type="button" variant="primary-dopamine" className="primary-button" onClick={handleReview}>
-          Проверить заказ
+          Следующий шаг
         </AnimatedButton>
       </div>
     </div>
